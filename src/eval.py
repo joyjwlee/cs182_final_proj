47039c5fed2d0b9f232c9a123a353a78868f8442
Phillip Chen <phillipe1042003@gmail.com>
2025-05-03 00:14:13 -0700

    [EVAL] Setup environment.yml for Windows and starting to build eval script

--- src/eval.py
+++ src/eval.py
@@ -360,34 +360,36 @@ def read_run_dir(run_dir):
     for task in os.listdir(run_dir):
         task_dir = os.path.join(run_dir, task)
         for run_id in os.listdir(task_dir):
-            run_path = os.path.join(task_dir, run_id)
-            _, conf = get_model_from_run(run_path, only_conf=True)
-            params = {}
-            params["run_id"] = run_id
-            params["task"] = task
-            params["model"] = conf_to_model_name(conf)
-            params["kwargs"] = "_".join(
-                f"{k}={v}" for k, v in conf.training.task_kwargs.items()
-            )
-            num_tasks = (
-                conf.training.num_tasks if "num_tasks" in conf.training else None
-            )
-            params["num_tasks"] = num_tasks if num_tasks is not None else -1
-            num_examples = (
-                conf.training.num_training_examples
-                if "num_training_examples" in conf.training
-                else None
-            )
-            params["num_examples"] = num_examples if num_examples is not None else -1
-            params["n_dims"] = conf.model.n_dims
-            params["n_layer"] = conf.model.n_layer
-            params["n_head"] = conf.model.n_head
-            params["run_name"] = conf.wandb.name
+            run_id_dir = os.path.join(task_dir, run_id)
+            for model in os.list_dir(run_id_dir):
+                run_path = os.path.join(run_id_dir, model)
+                _, conf = get_model_from_run(run_path, only_conf=True)
+                params = {}
+                params["run_id"] = run_id
+                params["task"] = task
+                params["model"] = conf_to_model_name(conf)
+                params["kwargs"] = "_".join(
+                    f"{k}={v}" for k, v in conf.training.task_kwargs.items()
+                )
+                num_tasks = (
+                    conf.training.num_tasks if "num_tasks" in conf.training else None
+                )
+                params["num_tasks"] = num_tasks if num_tasks is not None else -1
+                num_examples = (
+                    conf.training.num_training_examples
+                    if "num_training_examples" in conf.training
+                    else None
+                )
+                params["num_examples"] = num_examples if num_examples is not None else -1
+                params["n_dims"] = conf.model.n_dims
+                params["n_layer"] = conf.model.n_layer
+                params["n_head"] = conf.model.n_head
+                params["run_name"] = conf.wandb.name
 
-            for k, v in params.items():
-                if k not in all_runs:
-                    all_runs[k] = []
-                all_runs[k].append(v)
+                for k, v in params.items():
+                    if k not in all_runs:
+                        all_runs[k] = []
+                    all_runs[k].append(v)
 
     df = pd.DataFrame(all_runs).sort_values("run_name")
     assert len(df) == len(df.run_name.unique())
